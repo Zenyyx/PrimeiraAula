9041e1ee1779e5f88253aed6bdbc0850d2fcf170
Enzo <acount.of.games@gmail.com>
2018-09-20 22:37:20 -0300

    Primeira Aula de Java

---
+++ PrimeiroPrograma/src/First_Class.java
@@ -0,0 +1,17 @@
+
+public class First_Class {
+
+    public static void main(String[] args) {
+        //tipos de dados
+        char letra = 'a';
+        String texto = "texto";
+        
+        //logico
+        boolean valor = true;
+        
+        //numerico
+        int inteiro = 10;
+        float real = 10.0f;
+        double realDobrado = 20.00f;
+    }
+}
